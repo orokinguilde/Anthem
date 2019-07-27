f27bf06620eb19f52567fe2b9518dedd26bad32e
Akamelia <akamelia.clara@protonmail.com>
2019-07-28 00:12:42 +0200

    Moved from worker to web

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const moment = require('moment-timezone');
 const fs = require('fs');
 
+const app = require('express').createServer();
+const io = require('socket.io').listen(app);
+
 moment.locale('fr')
 
 process.env = fs.existsSync('./env.json') ? JSON.parse(fs.readFileSync('./env.json')) : process.env;
@@ -14,6 +17,8 @@ const bot = new Bot({
 bot.debug = fs.existsSync('./env.json');
 
 console.log('Debug mode:', bot.debug ? 'on' : 'off');
+
+const socket = io.listen(process.env.PORT);
 
 initialize(bot);
 
